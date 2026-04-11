56c02593bff55322de238ccfcf01a0a0cb07cb1d
Thomas W. Gantz <thomas.gantz@gmail.com>
2026-04-11 20:50:11 +0900

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 (function() {
   var container = document.getElementById('site-footer');
   if (container) {
-    container.innerHTML = '&copy; 2025&ndash;2026 Synthience Institute';
+    container.innerHTML = '&copy; 2025&ndash;2026 Synthience Institute &nbsp;&middot;&nbsp; <a href="#" style="color:#5a3e2b;text-decoration:none;font-size:0.85rem;" onmouseover="this.style.textDecoration=\'underline\'" onmouseout="this.style.textDecoration=\'none\'">&#8593; Back to top</a>';
   }
 })();
